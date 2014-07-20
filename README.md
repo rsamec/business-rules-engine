5fbc872b077dfc24f9813ebae048db8e1a346d6a
rsamec <roman.samec2@gmail.com>
2014-07-20 14:42:28 +0200

    bower fix ignore list

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
 # form
 
-form validation module
+Validation module
 
 [![build status](https://secure.travis-ci.org/rsamec/form.png)](http://travis-ci.org/rsamec/form)
 
 ## Installation
+
+This module is installed via bower:
+
+``` bash
+$ bower install form
+```
+
+## Example Usage
+
+Add reference to dist/validation.js file.
+
+``` js
+          //create new validator for object with structure<IPerson>
+          var personValidator = new Validation.AbstractValidator();
+
+          //basic validators
+          var required =new f.Validation.RequiredValidator();
+          var maxLength = new f.Validation.MaxLengthValidator(15);
+
+          //assigned validators to property
+          personValidator.RuleFor("FirstName", required);
+          personValidator.RuleFor("FirstName",maxLength);
+
+          //assigned validators to property
+          personValidator.RuleFor("LastName", required);
+          personValidator.RuleFor("LastName",maxLength);
+
+          personValidator = personValidator.CreateRule("Person");
+
+          var result = this.PersonValidator.Validate(this.Data);
+          if (result.HasErrors){
+              console.log(result.ErrorMessage);
+          }
+```
 
 This module is installed via npm:
 
---
+++ dist/localization/messages_cs.js
@@ -0,0 +1,39 @@
+///<reference path='../typings/underscore/underscore.d.ts'/>
+///<reference path='../validation/rules.ts'/>
+var Validation;
+(function (Validation) {
+    /*
+    * Translated default messages for the validation engine.
+    * Locale: CS (Czech; čeština, český jazyk)
+    */
+    _.extend(MessageLocalization.ValidationMessages, {
+        required: "Tento údaj je povinný.",
+        remote: "Prosím, opravte tento údaj.",
+        email: "Prosím, zadejte platný e-mail.",
+        url: "Prosím, zadejte platné URL.",
+        date: "Prosím, zadejte platné datum.",
+        dateISO: "Prosím, zadejte platné datum (ISO).",
+        number: "Prosím, zadejte číslo.",
+        digits: "Prosím, zadávejte pouze číslice.",
+        creditcard: "Prosím, zadejte číslo kreditní karty.",
+        equalTo: "Prosím, zadejte znovu stejnou hodnotu.",
+        extension: "Prosím, zadejte soubor se správnou příponou.",
+        maxlength: "Prosím, zadejte nejvíce {MaxLength} znaků.",
+        minlength: "Prosím, zadejte nejméně {MinLength} znaků.",
+        rangelength: "Prosím, zadejte od {MinLength} do {MaxLength} znaků.",
+        range: "Prosím, zadejte hodnotu od {Min} do {Max}.",
+        max: "Prosím, zadejte hodnotu menší nebo rovnu {Max}.",
+        min: "Prosím, zadejte hodnotu větší nebo rovnu {Min}.",
+        contains: "Prosím, zadejte hodnotu ze seznamu. Zadaná hodnota {AttemptedValue}.",
+        dateCompare: {
+            Format: "DD.MM.YYYY",
+            LessThan: "Prosím, zadejte datum menší než {CompareTo}.",
+            LessThanEqual: "Prosím, zadejte datum menší nebo rovné {CompareTo}.",
+            Equal: "Prosím, zadejte {CompareTo}.",
+            NotEqual: "Prosím, zadejte datum různé od {CompareTo}.",
+            GreaterThanEqual: "Prosím, zadejte datum větší nebo rovné {CompareTo}.",
+            GreaterThan: "Prosím, zadejte datum větší než {CompareTo}."
+        }
+    });
+})(Validation || (Validation = {}));
+//# sourceMappingURL=messages_cs.js.map

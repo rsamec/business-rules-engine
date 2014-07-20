78eb535fd9e5c2d6406f7110801f099957a614b2
rsamec <roman.samec2@gmail.com>
2014-07-20 14:11:40 +0200

    documentation

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,211 @@
+NG_DOCS={
+  "sections": {
+    "tutorial": "Tutorial",
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "tutorial",
+      "id": "index",
+      "shortName": "Tutorial",
+      "type": "overview",
+      "moduleName": "Tutorial",
+      "shortDescription": "Validation engine tutorial",
+      "keywords": "add advantages agnostic amd answer api application applications array-valued arrays assertions assigning async asynchronous automatically basic benefit build-in business check client collection collection-based collection-valued commonjs composite composition condition constrains constraints construction contract contracts create creditcard currency custom cz datacomparevalidator datecompare dateiso decorate depedency dependencies dependency dependend designed digits dom ease easily ecmascript email enables enforce engine equalto error extensible fields financial find framework generally generate generated great html independent introduced iterable javacript javascript js libraries library list lists localization luhn main max maxlength messages messages_cs min minlength module moment nested non-ui number object objects offers overview pattern plain product products promises properties property quick range rangelength reference repository representation required requires reused rule rules script server shared signeddigits step string structures supports temporary tight translateargs true trusted tutorial types typical ui underscore url usage validate validation validator validators validity walks work"
+    },
+    {
+      "section": "tutorial",
+      "id": "00_custom_rules",
+      "shortName": "1 - Custom property validator",
+      "type": "overview",
+      "moduleName": "1 - Custom property validator",
+      "shortDescription": "How to create a custom property validator.",
+      "keywords": "actual boolean class compare compareoperator compareto comparison create custom datecompare datecomparevalidator dates datetime days defaults dependencies diff diffs doc-tutorial-reset duration enables enum enumaration equal export false forces greater greaterthan greaterthanequal ignore ignoretime implement implements import inteface interface ipropertyvalidator isacceptable isdate isvalid lessthan lessthanequal method moment notequal notice operators overview parametrize passed property public require required result return rule set tagname time true tutorial undefined underscore utilities validation validator var"
+    },
+    {
+      "section": "tutorial",
+      "id": "01_property_rules",
+      "shortName": "2 - Custom object validator",
+      "type": "overview",
+      "moduleName": "2 - Custom object validator",
+      "shortDescription": "",
+      "keywords": "_rulefor_ _validationfor_ _validatorfor_ abstractvalidator aran args assign assigned async basic beforeeach checked child composition console correct create createrule custom data defer deferral describe doc-tutorial-nav enables equal errorinfo errormessage errors expect false fill firstname full function functions haserror haserrors image incorrect indexof interface iperson ischecked job jobs john jonh junior lastname learn list log logerrors long maxlength maxlengthvalidator named nezam object onespacefce onespaceforbidden options optionsfce overview param paramid paramvalidator person personvalidator program promise promiseresult properties property required requiredvalidator resolve result return returning rule rulefor rules selfvalidator settimeout shared simple smith space stava structure test text toooooooooooooooooooooooooooooooo true tutorial unknow validate validateasync validation validationfce validationfor validationresult validator validatorfce validators values var"
+    },
+    {
+      "section": "tutorial",
+      "id": "02_composition_rules",
+      "shortName": "3 - Composite validator",
+      "type": "overview",
+      "moduleName": "3 - Composite validator",
+      "shortDescription": "",
+      "keywords": "abstractvalidator adam async beforeeach bla call checked compose composite composition console contact contactvalidator correct countrycode countrycodes create createcontactvalidator createmainvalidator createpersonvalidator createphonevalidator createrule custom cze data defer deferral describe doc-tutorial-nav email emailvalidator equal error errormessage errors expect fill firstname fixedline fra function ger getdata haserrors icontact idata image incorrect interface iperson iphone john lastname learn log logerrors mail main mainvalidator match maxlength maxlengthvalidator mobile nested novak number object options optionsfce overview param paramid paramvalidator person person1 person2 personvalidator phonevalidator promise promiseresult property required requiredvalidator resolve result return rulefor rules selfvalidator settimeout smith structure structures sync test text tutorial usa validate validateasync validation validationresult validator validatorfor validators var"
+    },
+    {
+      "section": "tutorial",
+      "id": "03_list_rules",
+      "shortName": "4 - Validation rules with lists",
+      "type": "overview",
+      "moduleName": "4 - Validation rules with lists",
+      "shortDescription": "Validation rules for lists",
+      "keywords": "abstractvalidator adam arrays async beforeeach bla call checked compose composition console contact contactvalidator correct countrycode countrycodes create createcontactvalidator createmainvalidator createpersonvalidator createphonevalidator createrule custom cze data defer deferral describe email emailvalidator equal error errormessage errors expect fill firstname fixedline fra function ger getdata haserrors icontact idata image incorrect interface iperson iphone john lastname learn lists log logerrors mail main mainvalidator match maxlength maxlengthvalidator mobile nested novak number object options optionsfce overview param paramid paramvalidator person person1 person2 personvalidator phonevalidator promise promiseresult property repeated required requiredvalidator resolve result return rulefor rules selfvalidator settimeout smith structure structures sync test text tutorial usa validate validateasync validation validationresult validator validatorfor validators var"
+    },
+    {
+      "section": "tutorial",
+      "id": "04_localization_rules",
+      "shortName": "5 - Localization",
+      "type": "overview",
+      "moduleName": "5 - Localization",
+      "shortDescription": "",
+      "keywords": "abstractvalidator add anchor args arguments assigned assistant async attempted attemptedvalue automatically basic beforeeach birth birthdate break business case cel checked compareoperator compareto config construction containsvalidator createrule custom customerrormessage cz data datecompare datecomparevalidator datum day days dd default defaultmessages defer deferral describe doc-tutorial-nav en english enter equal error errormessage errors examples expect expectedmsg export extend false firstname format formatedattemptedvalue formatedcompareto full fullnameonespace function functions greater greaterthan greaterthanequal greter haserror help hodnota hodnotu ierrortranslateargs indexof interface interpolation je jednu jm job john junior languages lastname learn lessthan lessthanequal list localizate localization localize logerrors long lowerorequalthantoday man manualy maxlength maxlengthvalidator men message messageargs messagelocalization messages messsage mezeru mm moment msg named ne nebo nejv notequal object obsahovat od onespacefce onespaceforbidden options optionsfce overview param person personvalidator pouze povinn programmer promise promiseresult properties property pros replace required requiredvalidator resolve result return rovn rulefor rules set settimeout seznamu shared shop simple smith space string stringfce structure switch table tagname tento todate toooooooooooooooooooooooooooooooo translateargs translateid translation true tutorial typically undefined unemployed unknow validate validateasync validation validationfailures validationfce validationfor validationresult validator validatorfce validators values var worker write yyyy zadan zadejte ze znak"
+    },
+    {
+      "section": "api",
+      "id": "AbstractListValidationRule",
+      "shortName": "AbstractListValidationRule",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents an validator for custom object. It enables to assign rules to custom object properties.",
+      "keywords": "abstractlistvalidationrule api assign custom enables object properties represents rules validation validator"
+    },
+    {
+      "section": "api",
+      "id": "AbstractValidationRule",
+      "shortName": "AbstractValidationRule",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents concreate validator for custom object. It enables to assign validation rules to custom object properties.",
+      "keywords": "abstractvalidationrule api assign concreate custom enables object properties represents rules validation validator"
+    },
+    {
+      "section": "api",
+      "id": "AbstractValidator",
+      "shortName": "AbstractValidator",
+      "type": "object",
+      "moduleName": "AbstractValidator",
+      "shortDescription": "It enables to create custom validator for your own object and to assign validation rules to its properties.",
+      "keywords": "_rulefor_ _validationfor_ _validatorfor_ abstractvalidator api assign assigned async child composition create custom enables object properties property rules shared validation validator validators"
+    },
+    {
+      "section": "api",
+      "id": "AbstractValidator",
+      "shortName": "AbstractValidator",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It enables to create custom validator for your own abstract object (class) and to assign validation rules to its properties.",
+      "keywords": "_rulefor_ _validationfor_ _validatorfor_ abstract abstractvalidator api assign assigned async child composition create custom enables object properties property rules shared validation validator validators"
+    },
+    {
+      "section": "api",
+      "id": "CompositeValidationResult",
+      "shortName": "CompositeValidationResult",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents composite error object.",
+      "keywords": "api composite compositevalidationresult error object represents validation"
+    },
+    {
+      "section": "api",
+      "id": "DateCompareValidator",
+      "shortName": "DateCompareValidator",
+      "type": "object",
+      "moduleName": "DateCompareValidator",
+      "shortDescription": "DateCompareValidator enables to compare date to another date (CompareTo).",
+      "keywords": "actual api class compare compareoperator compareto comparison datecomparevalidator datecomparevalidator-page datetime days enables forces ignore ignoredate isacceptable lessthanequal moment month object property result return set time true underscore validation validator var"
+    },
+    {
+      "section": "api",
+      "id": "Error",
+      "shortName": "Error",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents basic error structure.",
+      "keywords": "api basic error object represents structure validation"
+    },
+    {
+      "section": "api",
+      "id": "ICOValidator",
+      "shortName": "ICOValidator",
+      "type": "object",
+      "moduleName": "ICOValidator",
+      "shortDescription": "Return true for valid identification number of CZE company (called ico), otherwise return false.",
+      "keywords": "api company cze false ic ico icovalidator identification isacceptable number object result return true valid validator var"
+    },
+    {
+      "section": "api",
+      "id": "ParamValidator",
+      "shortName": "ParamValidator",
+      "type": "object",
+      "moduleName": "ParamValidator",
+      "shortDescription": "Returns true if the value is present in the list. Otherwise return false.",
+      "keywords": "academic agriculture api assistant class countries cze czech defer deferral equal existing expect false france function germany goverment isacceptable item items jobs linenums list machinery manager object options optionsfce pairs param parameter parametrize paramid paramvalidator paramvalidator-page prettyprint programmer promise promiseresult promiseresult2 property republic resolve result return returned returns settimeout shop text true unemployed validator var"
+    },
+    {
+      "section": "api",
+      "id": "PropertyValidationRule",
+      "shortName": "PropertyValidationRule",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents a property validation rule. The property has assigned collection of property validators.",
+      "keywords": "api assigned collection object property propertyvalidationrule represents rule validation validators"
+    },
+    {
+      "section": "api",
+      "id": "RCValidator",
+      "shortName": "RCValidator",
+      "type": "object",
+      "moduleName": "RCValidator",
+      "shortDescription": "Return true for valid birth day number in Czech Republic, otherwise return false.",
+      "keywords": "api birth czech day false isacceptable moment number object rc rcvalidator republic result return string true underscore valid validator var"
+    },
+    {
+      "section": "api",
+      "id": "ValidationContext",
+      "shortName": "ValidationContext",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents a data context for validation rule.",
+      "keywords": "api context data object represents rule validation validationcontext"
+    },
+    {
+      "section": "api",
+      "id": "ValidationFailure",
+      "shortName": "ValidationFailure",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents validation failure.",
+      "keywords": "api failure object represents validation validationfailure"
+    },
+    {
+      "section": "api",
+      "id": "ValidationResult",
+      "shortName": "ValidationResult",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents simple abstract error object.",
+      "keywords": "abstract api error object represents simple validation validationresult"
+    },
+    {
+      "section": "api",
+      "id": "Validator",
+      "shortName": "Validator",
+      "type": "object",
+      "moduleName": "Validation",
+      "shortDescription": "It represents a custom validator. It enables to define your own shared validation rules",
+      "keywords": "api custom define enables object represents rules shared validation validator"
+    }
+  ],
+  "apis": {
+    "tutorial": false,
+    "api": true
+  },
+  "html5Mode": true,
+  "startPage": "/api",
+  "discussions": {
+    "shortName": "my",
+    "url": "http://my-domain.com",
+    "dev": false
+  },
+  "scripts": [
+    "angular.min.js"
+  ]
+};
